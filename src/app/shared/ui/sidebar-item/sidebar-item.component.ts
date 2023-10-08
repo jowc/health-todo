056153bdb9c40974324fbd84c469167a2b9d86b2
Joseph Chikeme <jchikeme@gmail.com>
2023-10-08 18:08:59 +0100

    worked on sidebar component

--- src/app/shared/ui/sidebar-item/sidebar-item.component.ts
+++ src/app/shared/ui/sidebar-item/sidebar-item.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-sidebar-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './sidebar-item.component.html',
-  styleUrls: ['./sidebar-item.component.scss']
+  styleUrls: ['./sidebar-item.component.scss'],
 })
 export class SidebarItemComponent {
-
+  @Input({ required: true }) itemData!: {
+    icon: IconDefinition;
+    title: string;
+    count: number;
+    active?: boolean;
+  };
 }
--- src/app/shared/ui/sidebar/sidebar.component.ts
+++ src/app/shared/ui/sidebar/sidebar.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { SidebarItemComponent } from '../sidebar-item/sidebar-item.component';
+import {
+  faCalendarDay,
+  faCalendarWeek,
+  faCheck,
+  faFlag,
+} from '@fortawesome/free-solid-svg-icons';
+import { faFolderOpen } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [SidebarItemComponent],
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
-
+  menuItems = signal([
+    {
+      icon: faCalendarDay,
+      title: 'Today',
+      count: 5,
+      active: true,
+    },
+    {
+      icon: faCalendarWeek,
+      title: 'Scheduled',
+      count: 5,
+      active: false,
+    },
+    {
+      icon: faFolderOpen,
+      title: 'All',
+      count: 5,
+      active: false,
+    },
+    {
+      icon: faFlag,
+      title: 'Flaged',
+      count: 5,
+      active: false,
+    },
+    {
+      icon: faCheck,
+      title: 'Completed',
+      count: 5,
+      active: false,
+    },
+  ]);
 }
