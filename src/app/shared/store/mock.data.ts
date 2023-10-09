d9ccdab6fd3cab4f48c98bbc71cd42c245db0585
Joseph Chikeme <jchikeme@gmail.com>
2023-10-09 18:04:56 +0100

    completed add task form

--- src/app/shared/store/mock.data.ts
+++ src/app/shared/store/mock.data.ts
@@ -3,6 +3,7 @@ export interface taskInterface {
   task: string;
   description: string;
   time: string;
+  priority: boolean;
 }
 
 export const tasks = [
@@ -11,29 +12,34 @@ export const tasks = [
     task: 'Clean the room',
     description: 'Include the living room and parking store.',
     time: '9:00 pm',
+    priority: false,
   },
   {
     id: 2,
-    task: 'Clean the room',
-    description: 'Include the living room and parking store.',
-    time: '9:00 pm',
+    task: 'Finish code test',
+    description: 'Complete code test and save on public repo.',
+    time: '13:00 pm',
+    priority: true,
   },
   {
     id: 3,
-    task: 'Clean the room',
-    description: 'Include the living room and parking store.',
-    time: '9:00 pm',
+    task: 'Send completed test to company',
+    description: 'Deploy code test and share the url.',
+    time: '20:00 pm',
+    priority: false,
   },
   {
     id: 4,
-    task: 'Clean the room',
-    description: 'Include the living room and parking store.',
-    time: '9:00 pm',
+    task: 'Take a break and continue learning',
+    description: 'Continue my courses on Youtube.',
+    time: '1:00 am',
+    priority: false,
   },
   {
     id: 5,
-    task: 'Clean the room',
-    description: 'Include the living room and parking store.',
-    time: '9:00 pm',
+    task: 'Add two material dialogs',
+    description: 'Add and edit task dialogs with passed data.',
+    time: '5:00 am',
+    priority: false,
   },
 ];
--- src/app/ui/table/table.component.ts
+++ src/app/ui/table/table.component.ts
@@ -1,29 +1,53 @@
 import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
 import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
-import { faPlus, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faPlus, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { MockDataService } from 'src/app/shared/store/mock-data.service';
 import { taskInterface } from 'src/app/shared/store/mock.data';
+import { AddTaskModalComponent } from '../add-task-modal/add-task-modal.component';
+import { EditTaskModalComponent } from '../edit-task-modal/edit-task-modal.component';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+    AddTaskModalComponent,
+    EditTaskModalComponent,
+    MatDialogModule,
+  ],
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
 })
 export class TableComponent {
   readonly dataService = inject(MockDataService);
+  private readonly dialog = inject(MatDialog);
   faEdit = signal(faPenToSquare);
   faDelete = signal(faTrashCan);
   faPlus = signal(faPlus);
+  faCheck = signal(faCheck);
 
   trackTask(index: number, task: taskInterface) {
     return task.id;
   }
 
-  editTask = (task: taskInterface) => this.dataService.editTask(task);
+  addTask() {
+    return this.dialog.open(AddTaskModalComponent, {
+      height: '520px',
+      width: '500px',
+    });
+  }
+
+  editTask = (task: taskInterface) =>
+    this.dialog.open(EditTaskModalComponent, {
+      data: task,
+      height: '520px',
+      width: '550px',
+    });
 
   deleteTask = (id: number) => this.dataService.deleteTask(id);
 }
