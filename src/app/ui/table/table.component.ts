f657c1c234841ed9d94eb1e98f2e998f8d80fd20
Joseph Chikeme <jchikeme@gmail.com>
2023-10-10 19:10:30 +0100

    UI/UX updates

--- src/app/ui/table/table.component.ts
+++ src/app/ui/table/table.component.ts
@@ -1,4 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+  inject,
+  signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
@@ -9,6 +17,8 @@ import { MockDataService } from 'src/app/shared/store/mock-data.service';
 import { taskInterface } from 'src/app/shared/store/mock.data';
 import { AddTaskModalComponent } from '../add-task-modal/add-task-modal.component';
 import { EditTaskModalComponent } from '../edit-task-modal/edit-task-modal.component';
+import { Subscription, timer } from 'rxjs';
+import { DialogStateService } from '../utils/dialog-state.service';
 
 @Component({
   selector: 'app-table',
@@ -23,16 +33,40 @@ import { EditTaskModalComponent } from '../edit-task-modal/edit-task-modal.compo
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
 })
-export class TableComponent {
+export class TableComponent implements OnInit, OnDestroy {
   readonly dataService = inject(MockDataService);
   private readonly dialog = inject(MatDialog);
+  private dialogState = inject(DialogStateService);
+  @ViewChild('table', { static: true })
+  tableBody!: ElementRef<HTMLTableElement>;
+  sub!: Subscription;
   faEdit = signal(faPenToSquare);
   faDelete = signal(faTrashCan);
   faPlus = signal(faPlus);
   faCheck = signal(faCheck);
 
+  ngOnInit(): void {
+    this.sub = this.dialogState.dialogClose.subscribe((modal) => {
+      if (modal) this.scrollToLatest();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+    this.scrollToLatest().unsubscribe();
+  }
+
   trackTask(index: number, task: taskInterface) {
     return task.id;
+  }
+
+  scrollToLatest() {
+    console.log('invoked');
+    return timer(300).subscribe(() => {
+      console.log('invoked timer');
+      const height = this.tableBody.nativeElement.scrollHeight;
+      return (this.tableBody.nativeElement.scrollTop = height);
+    });
   }
 
   addTask() {
